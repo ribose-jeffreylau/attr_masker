9f4f2d8af385e52e8c5e598ee890dbc9df6b6fd9
Jeffrey Lau <jeffrey.lau@ribose.com>
2021-12-22 15:50:39 +0800

    test: Debug WITHOUT_ACTIVE_RECORD

--- spec/support/00_control_constants.rb
+++ spec/support/00_control_constants.rb
@@ -1,2 +1,14 @@
 WITHOUT_ACTIVE_RECORD = ENV.fetch("WITHOUT", "") =~ /\bactiverecord\b/
 WITHOUT_MONGOID = ENV.fetch("WITHOUT", "") =~ /\bmongoid\b/
+
+require 'pp'
+
+puts "... What is WITHOUT_ACTIVE_RECORD?, #{WITHOUT_ACTIVE_RECORD.inspect}"
+warn "... What is WITHOUT_ACTIVE_RECORD?, #{WITHOUT_ACTIVE_RECORD.inspect}"
+pp WITHOUT_ACTIVE_RECORD
+
+puts
+
+puts "... What is WITHOUT_MONGOID?, #{WITHOUT_MONGOID.inspect}"
+warn "... What is WITHOUT_MONGOID?, #{WITHOUT_MONGOID.inspect}"
+pp WITHOUT_MONGOID
