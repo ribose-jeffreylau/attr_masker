3308cea264e056d50ea33124a1a649efb435a195
Jeffrey Lau <jeffrey.lau@ribose.com>
2021-12-23 14:08:34 +0800

    wip: Debug combustion WITHOUT_ACTIVE_RECORD

--- spec/support/20_combustion.rb
+++ spec/support/20_combustion.rb
@@ -3,6 +3,16 @@
 
 Combustion.path = "spec/dummy"
 
+require 'pp'
+
+puts "support/combu: What is WITHOUT_ACTIVE_RECORD?, #{WITHOUT_ACTIVE_RECORD.inspect}"
+pp WITHOUT_ACTIVE_RECORD
+
+puts
+
+puts "support/combu: What is WITHOUT_MONGOID?, #{WITHOUT_MONGOID.inspect}"
+pp WITHOUT_MONGOID
+
 if WITHOUT_ACTIVE_RECORD
   Combustion.initialize!
 else
